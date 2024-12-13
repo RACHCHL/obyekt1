75af2d7cd0c38d14b60e654b76dc918d064601da
Rasulbek <quvondiqovrasulbek@gmail.com>
2024-12-14 01:45:22 +0500

    master

---
+++ main.js
@@ -0,0 +1,35 @@
+for (let i = 0; i < 3; i++) {
+    const obyekt ={
+        name: "ism",
+        ago: "yosh"
+        
+    };
+    obyekt.name = prompt("Isim kiriting")
+    obyekt.ago = + prompt("Yoshini kiriting")    
+    for (const key in obyekt) {
+    
+    console.log(`Foydalanuvchi ${i+1}`);
+    console.log( ` Ismi ${obyekt.name}`);
+    console.log(` Yoshi ${obyekt.ago}`) ;
+    }
+   }  
+     
+       
+    
+   
+
+
+        
+        
+        
+    
+            
+
+    
+    
+
+    
+
+    
+
+
